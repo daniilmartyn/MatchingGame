751c9ff46824ec910dc2bb58ff0078413bbe5b65
Daniil Sergeyevich Martyn <daniil.martyn@wsu.edu>
2017-07-18 22:47:00 -0700

    Game Complete. There are no start/reset controls. To begin game, open app. When the game is won, the app closes.

--- MatchingGame/MatchingGame/Form1.cs
+++ MatchingGame/MatchingGame/Form1.cs
@@ -12,9 +12,105 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+
+        Label firstClicked = null;
+        Label secondClicked = null;
+
+        Random random = new Random();
+
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconlabel = control as Label;
+                if (iconlabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconlabel.Text = icons[randomNumber];
+                    iconlabel.ForeColor = iconlabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+
+        private void label_click(object sender, EventArgs e)
+        {
+            Label clickedLabel = sender as Label;
+
+            if (timer1.Enabled)
+                return;
+
+            if(clickedLabel != null)
+            {
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    clickedLabel.ForeColor = Color.Black;
+                }
+                else
+                {
+
+                    secondClicked = clickedLabel;
+                    secondClicked.ForeColor = Color.Black;
+                    
+
+                    if (firstClicked.Text == secondClicked.Text)
+                    {
+                        firstClicked = null;
+                        secondClicked = null;
+                        checkForWinner();
+                        return;
+                    }
+
+                    timer1.Start();
+                }
+            }
+        }
+
+        private void checkForWinner()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if(iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+
+                MessageBox.Show("You did it you memory master you!", "Hooray!!");
+                Close();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
+
+
         }
     }
 }
